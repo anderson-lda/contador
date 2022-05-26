c68730f1e3977ab78a2bca1b81bc1eb9a7f98b19
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-05-26 14:34:00 -0300

    array of tasks

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,18 +2,24 @@ import React from "react";
 
 //forma mais moderna de se criar componentes
 export function Lista(){
+    const tarefas = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    },{
+        tarefa: 'JavaScript',
+        tempo: '01:30:00'
+    }]
     return(
         <aside>
             <h2>Tarefas do Dia</h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>02:00:00</span>
-                </li>
-                <li><h3>JavasScript</h3>
-                <span>01:00:00</span></li>
+                {tarefas.map((item, index)=>(//() para retornar diretamente;{} indica escopo
+                    <li key="index">{/*recomendado uma key para o react saber distinguir qual item se está trabalhando */}
+                        <h3>{item.tarefa}</h3>
+                        <span>{item.tempo}</span>
+                    </li>
+                ))}
+                
             </ul>
         </aside>
     )
