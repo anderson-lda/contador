8a3df3fdda37c8dacc6df4bcf4b4e3cdb11a520c
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-05-27 23:12:12 -0300

    formulario and botao as function

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import style from './Botao.module.scss'
 
+//feito como function
+/*interface Props{
+    type?: "button" | "submit" | "reset" | undefined, 
+    onClick?: () => void,
+    children?: React.ReactNode
+}
+function Botao({onClick,type, children}: Props){
+    return(
+        <button onClick={onClick} type={type} className={style.botao}>
+            {children}
+        </button>
+    )
+}*/
+
 class Botao extends React.Component<{ 
     children: any, 
     type?: "button" | "submit" | "reset" | undefined, onClick?: () => void }> {
--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,8 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import style from './Formulario.module.scss'
 import { v4 as uuidv4 } from 'uuid'
+
+//Formulario como function
+/*interface Props{
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}
+function Formulario({ setTarefas}: Props){
+    const [tarefa, setTarefa] = useState("");
+    const [tempo, setTempo] = useState("00:00")
+    function adicionarTarefa(evento:React.FormEvent<HTMLFormElement>){ //formevent: evento de formulário;<HTML.. opcional
+        evento.preventDefault();
+        setTarefas(tarefasAntigas => 
+            [
+                ...tarefasAntigas,
+                {
+                    tarefa,
+                    tempo,
+                    selecionado: false,
+                    completado: false,
+                    id: uuidv4() //funcao que retorna id aleatorio
+                }
+            ]);
+            setTarefa("")
+            setTempo("00:00")
+    }
+    return(
+        <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
+                <div className={style.inputContainer}>
+                    <label htmlFor="tarefa">Adicione uma nova tarefa</label>
+                    <input 
+                    type="text" 
+                    name="tarefa" 
+                    value={tarefa}
+                    onChange={e=>setTarefa(e.target.value)}
+                    id="tarefa"
+                    placeholder="Digite sua tarefa" 
+                    required
+                    />
+                </div>
+                <div className={style.inputContainer}>
+                    <label htmlFor="tempo">Tempo</label>
+                    <input 
+                    type="time"
+                    step="1"
+                    name="tempo"
+                    value={tempo}
+                    onChange={evento => setTempo(evento.target.value)}
+                    id="tempo"
+                    min="00:00:00"
+                    max="01:30:00"
+                    required
+                    />
+                </div>
+                <Botao type="submit">Adicionar</Botao>
+            </form>
+    )
+}*/
 
 class Formulario extends React.Component<{setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>}> {
     state = { //para class components não há hooks
